3720d31877335ad625bcf666f006ec01a67b4a3d
Matias Sayago <matiasgd99@gmail.com>
2023-10-17 16:11:23 -0300

    Initialize

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(
+    PORT, () => {
+        console.log(`Servidor escuchando en el puerto ${PORT}`);
+    }
+)
+
+app.get(
+    "/home", (req, res) => {
+        res.sendFile(path.join(__dirname,))
+    }
+)
