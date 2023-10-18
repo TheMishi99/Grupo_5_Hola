ac34316251ca3cea051a208c56067666c8f80f6d
Matias Sayago <matiasgd99@gmail.com>
2023-10-18 13:39:27 -0300

    Mishi added this

--- src/app.js
+++ src/app.js
@@ -16,21 +16,3 @@ app.get(
         res.sendFile(path.join(__dirname, "views", "index.html"));
     }
 )
-
-app.get(
-    "/shopping-cart", (req, res) => {
-        res.sendFile(path.join(__dirname, "views", "shopping-cart.html"));
-    }
-)
-
-app.get(
-    "/login", (req, res) => {
-        res.sendFile(path.join(__dirname, "views", "login.html"));
-    }
-)
-
-app.get(
-    "/sign-up", (req, res) => {
-        res.sendFile(path.join(__dirname, "views", "sign-up.html"));
-    }
-)
