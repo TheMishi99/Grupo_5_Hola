47a06a6b6902793b06a96ed54c30ff4e4b87950b
joacobolzon <joaco.bolzon3@gmail.com>
2023-10-31 02:37:50 -0300

    app.js static fixed

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const PORT = 5000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src/public")));
 
-app.listen(
-    PORT, () => {
-        console.log(`Servidor escuchando en el puerto ${PORT}`);
-    }
-)
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
 
-app.get(
-    "/home", (req, res) => {
-        res.sendFile(path.join(__dirname, "src", "views", "index.html"));
-    }
-)
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "views", "index.html"));
+});
