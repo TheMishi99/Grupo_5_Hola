287be91fbccc6aa0dbf0128d1a99bf880c8c4dee
Enzo Joaquin Bolzon <joaco.bolzon3@gmail.com>
2023-11-21 20:59:29 -0300

    createProduct

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,15 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const productsController = require("../controllers/productsController")
+const productsController = require("../controllers/productsController");
 
-router.get("/cart", productsController.cart)
-router.get("/details", productsController.detail)
+router.get("/cart", productsController.cart);
+router.get("/details", productsController.detail);
 
-module.exports = router
+router.get("/create", productsController.createView);
+router.post("/create", productsController.create);
+
+/* router.get("/modify/:id", productsController.modifyView);
+router.put("/modify/:id", productsController.create); */
+
+module.exports = router;
