c5f6b1dee28746cec8845bca9d2c32c178b56c4b
Enzo Joaquin Bolzon <joacobolzon3@gmail.com>
2024-01-21 04:20:42 -0300

    users-database-fixed

--- src/database/migrations/20240114064516-usuarios.js
+++ src/database/migrations/20240114064516-usuarios.js
@@ -30,7 +30,7 @@ module.exports = {
         allowNull: false
       },
       authLevel: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false
       },
       active: {
