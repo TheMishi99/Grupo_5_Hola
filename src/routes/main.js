203ec3f45e312be6217876229bf187abe1bb88fa
lucianafigue <mlfluli@gmail.com>
2024-01-25 14:52:20 -0300

    Incorporaciones de vista, estilo y controller

--- src/routes/main.js
+++ src/routes/main.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const mainController = require("../controllers/mainController")
 
 router.get("/", mainController.index)
+router.get("/stores", mainController.stores)
 
 module.exports = router
