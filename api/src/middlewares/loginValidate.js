8dad868601890e9b84911c5d0b941d4b85ffc284
Denise Agostina Geraldine Diaz <dgeraldineunviersidad@gmail.com>
2024-03-21 20:42:42 -0300

    Notas Geraldine

--- api/src/middlewares/loginValidate.js
+++ api/src/middlewares/loginValidate.js
@@ -1,14 +1,15 @@
 const User = require("../models/user-model");
 
-const isLogged = async (req, res, next) => {
+//se  define un middleware llamado isLogged que verifica si un usuario está logueado.
+
+const isLogged = async (req, res, next) => { // funcion que toma tres parámetros: el el objeto solicitud, el objeto respuesta y la función de paso al siguiente middleware).
   let userToLogin;
-  if(req.session.isLogged){
-    userToLogin = await User.findByField("email", req.session.isLogged.email);
+  if(req.session.isLogged){ // Comprueba si existe una sesión activa.
+    userToLogin = await User.findByField("email", req.session.isLogged.email); // Con findByField se busca un usuario en la base de datos de acuerdo al gmail.
     if (userToLogin) {
-      return res.redirect(`/users/${userToLogin.id}/profile`);
+      return res.redirect(`/users/${userToLogin.id}/profile`); //Si se encuentra el usuario en la base de datos, se redirige al usuario a su perfil.
     }
   }
-
   next();
 };
 
--- api/src/middlewares/registerValidate.js
+++ api/src/middlewares/registerValidate.js
@@ -3,14 +3,18 @@ const path  = require("path");
 // const userModel = require("../models/user-model");
 const db = require("../database/models");
 
+// Validación para un formulario de registro utilizando Express.js y Sequelize.
+
+// Creamos un array  que contiene una serie de validaciones para diferentes campos del formulario de registro.
 const registerValidate = [
-    body('name')
-        .notEmpty().withMessage("Debes completar este campo").bail()
-        .isLength({ min: 2 }).withMessage("Debes completar con un nombre y apellido válido"),
+    //body proviene de express-validator y valida el campo de nombre del formulario.
+    body('name') 
+        .notEmpty().withMessage("Debes completar este campo").bail() //no vacio
+        .isLength({ min: 2 }).withMessage("Debes completar con un nombre y apellido válido"), //minimo 2 caracteres
     body('email')
-        .notEmpty().withMessage("Debes completar este campo").bail()
-        .isEmail().withMessage("Introduzca una dirección de correo electrónico válida").bail()
-        .custom(async (value, {req}) =>{
+        .notEmpty().withMessage("Debes completar este campo").bail() //no vacio
+        .isEmail().withMessage("Introduzca una dirección de correo electrónico válida").bail() // validar que sea email
+        .custom(async (value, {req}) =>{ // validacion personalizada para ver si el email ya está registrado.
             let userInDB = await db.Usuarios.findOne({
                 where: {
                     email: req.body.email
@@ -25,10 +29,10 @@ const registerValidate = [
         .notEmpty().withMessage("Debes completar este campo").bail()
         .isLength({ min: 8 }).withMessage("La contraseña debe tener al menos 8 caracteres")
         .isLength({ max: 20 }).withMessage("La contraseña debe tener como máximo 20 caracteres")
-        .matches(/^(?=.*[A-Z]{1,})(?=.*[a-z]{1,})(?=.*\d{1,})(?=.*\W{1,})+/).withMessage('La contraseña debe contener al menos un número, una mayúscula, una minúscula y un carácter especial'),
+        .matches(/^(?=.*[A-Z]{1,})(?=.*[a-z]{1,})(?=.*\d{1,})(?=.*\W{1,})+/).withMessage('La contraseña debe contener al menos un número, una mayúscula, una minúscula y un carácter especial'), // garantiza que la contraseña contenga al menos una mayúscula, una minúscula, un número y un carácter especial.
     body('confirmpassword')
         .notEmpty().withMessage("Debes completar este campo").bail()
-        .custom((value, { req }) => {
+        .custom((value, { req }) => { // validacion personalizada para ver si las contraseñas coinciden
             if (value !== req.body.password) {
                 throw new Error("Las contraseñas no coinciden")
             }
@@ -41,12 +45,12 @@ const registerValidate = [
         .custom((value, { req }) => {
             const file = req.file;
             const acceptedExtensions = ['.jpg','.png','.jpeg','.gif'];
-            if (!file) {
+            if (!file) { // verifica si se ha adjuntado un archivo 
                 throw new Error("Debes subir una imagen de perfil")
             }else{
                 const fileExtension = path.extname(file.originalname);
-                if(!acceptedExtensions.includes(fileExtension)){
-                    throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`)
+                if(!acceptedExtensions.includes(fileExtension)){ // valida la extensión del archivo y si se ha adjuntado un archivo al formulario.
+                    throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`) 
                 }
             }
             return true;
